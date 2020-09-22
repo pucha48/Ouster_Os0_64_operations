17286c03e6a499ee39a278381cacdce2a5424dd0
Tejal-gole <tejal3093@gmail.com>
2020-09-22 16:01:04 +0530

    obstacle and road plane

--- multiprocessing_trail.py
+++ multiprocessing_trail.py
@@ -1,9 +1,12 @@
 from os1 import OS1
-from os1.utils import xyz_points
+from os1.utils import xyz_points , build_trig_table
 import numpy as np
 import open3d as o3d
 import pcl
 import time
+import json
+from multiprocessing import Process, Queue
+
 from conti_plotting import plane_segmentation,roi_filter , clustering, get_cluster_box_list
 lines = [
         [0, 1],
@@ -27,6 +30,7 @@ fetch_mode = '1024x10'
 lidar_channels = 64
 resolution = 1024
 fnt =0
+unprocessed_packets = Queue()
 
 # init_sample_data = np.random.randn(16384 ,3)   # Random data
 init_sample_data = np.random.randn(65536 ,3)   # Random data
@@ -47,16 +51,36 @@ line_y = []
 line_z = []
 counter = 0
 
-def handler(raw_packet):
+
+def handler(packet):
+    unprocessed_packets.put(packet)
+
+
+def spawn_workers(n, worker, *args, **kwargs):
+    processes = []
+    for i in range(n):
+        process = Process(
+            target=worker,
+            args=args,
+            kwargs=kwargs
+        )
+        process.start()
+        processes.append(process)
+    return processes
+
+
+def worker(queue, beam_altitude_angles, beam_azimuth_angles):
     global line_x
     global line_y
     global line_z
     global counter
     global lines
     global fnt
+    build_trig_table(beam_altitude_angles, beam_azimuth_angles)
 
-    if 1:
-        x, y, z = xyz_points(raw_packet)
+    while True:
+        packet = queue.get()
+        x, y, z = xyz_points(packet)
         x = list(np.round(x,3))
         y = list(np.round(y,3))
         z = list(np.round(z,3))
@@ -73,18 +97,18 @@ def handler(raw_packet):
             # line_x_arr = line_x_arr[:,0::4]
             # line_z_arr = line_z_arr[:,0::4]
             # tp = np.flatten(line_x)
-            point_cloud_np =np.concatenate((np.transpose(line_x_arr),np.transpose(line_y_arr),np.transpose(line_z_arr)),axis=1)
+            point_cloud_np = np.concatenate((np.transpose(line_x_arr),np.transpose(line_y_arr),np.transpose(line_z_arr)),axis=1)
 
             ############################ PCL codeing ###################################################
             xyz = np.float32(point_cloud_np)
 
-            cloud_XYZ = pcl.PointCloud()
-            cloud_XYZ.from_array(xyz)
-            cloud_roi_filtered = roi_filter(cloud_XYZ, [-1, 50], [-2,2], [-2, 2])
-
-            indices, coefficients = plane_segmentation(cloud_roi_filtered, 0.1, 1000)
-
-            cloud_plane = cloud_roi_filtered.extract(indices, negative=True)
+            # cloud_XYZ = pcl.PointCloud()
+            # cloud_XYZ.from_array(xyz)
+            # cloud_roi_filtered = roi_filter(cloud_XYZ, [-1, 50], [-2,2], [-2, 2])
+            #
+            # indices, coefficients = plane_segmentation(cloud_XYZ, 0.1, 1000)
+            #
+            # cloud_plane = cloud_XYZ.extract(indices, negative=True)
 
             # cluster_indices = clustering(cloud_plane, 0.3, 30, 100)
             #
@@ -104,16 +128,10 @@ def handler(raw_packet):
             # if box_coord_list != []:
             # point_cloud_np = np.array(line_set)
             # point_cloud_np = np.array(point_cloud_np)
-            point_cloud_np = np.array(cloud_roi_filtered)
-            ##############################################################################################
-            # print("cloud data", point_cloud_np.shape)
-
+            # point_cloud_np = np.array(cloud_plane)
+                ################################################################
+            print("cloud data", point_cloud_np.shape)
             pcd.points = o3d.utility.Vector3dVector(point_cloud_np)
-            pcd.paint_uniform_color([0,1,0])
-            np.asarray(pcd.colors)[indices[0:], :] = [1, 0, 0]
-            print(np.asarray(pcd.colors).shape)
-            # np.asarray(pcd.colors)
-            # np.asarray(pcd.colors)[indices[1:], :] = [1, 0, 0]
             # file_name= "3d_point_"+str(fnt)+".png"
             # vis.capture_screen_image(file_name)
             vis.update_geometry()
@@ -124,9 +142,17 @@ def handler(raw_packet):
             line_y = []
             line_z = []
 
-os1 = OS1(sensor_ip, system_ip, mode=fetch_mode)  # OS1 sensor IP, destination IP, and resolution
-# Inform the sensor of the destination host and reintialize it
+
+os1 = OS1(sensor_ip, system_ip)
+beam_intrinsics = json.loads(os1.get_beam_intrinsics())
+beam_alt_angles = beam_intrinsics['beam_altitude_angles']
+beam_az_angles = beam_intrinsics['beam_azimuth_angles']
+workers = spawn_workers(4, worker, unprocessed_packets, beam_alt_angles, beam_az_angles)
 os1.start()
-os1.run_forever(handler)
+try:
+    os1.run_forever(handler)
+except KeyboardInterrupt:
+    for w in workers:
+        w.terminate()
 
 
